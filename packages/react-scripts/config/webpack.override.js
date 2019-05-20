2278a02249d88af1545f3184ab55286431d820a8
Torbjörn Kvist <torbjorn.kvist@indentive.se>
2019-05-20 07:46:47 +0200

    :wrench: missed return statement and cleanup.

--- packages/react-scripts/config/webpack.override.js
+++ packages/react-scripts/config/webpack.override.js
@@ -1,14 +1,16 @@
-const path = require('path')
+const path = require('path');
 
 const configFactory = require('./webpack.config');
 const paths = require('./paths');
 
 module.exports = function(webpackEnv) {
-  const overrideConfigPath = path.join(paths.appPath, 'webpack.override.js')
-  const config = configFactory(webpackEnv)
+  const overrideConfigPath = path.join(paths.appPath, 'webpack.override.js');
+  const config = configFactory(webpackEnv);
   try {
-    require.resolve(overrideConfigPath)
-    require(overrideConfigPath)(webpackEnv, configFactory(webpackEnv))
-  } catch(e) {}
-  return configFactory(webpackEnv)
+    require.resolve(overrideConfigPath);
+    return require(overrideConfigPath)(webpackEnv, configFactory(webpackEnv));
+  } catch(e) {
+     console.error('Webpack Override failed', e);
+  }
+  return configFactory(webpackEnv);
 };
